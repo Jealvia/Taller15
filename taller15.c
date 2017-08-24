947e14023fa5e318367cd742ee8b4b9ed398c62c
JRiascos1995 <jamin.r.m@hotmail.com>
2017-08-24 16:10:12 -0500

    final

--- taller15.c
+++ taller15.c
@@ -1,95 +1,52 @@
-/*
- * daemonize.c
- * This example daemonizes a process, writes a few log messages,
- * sleeps 20 seconds and terminates afterwards.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <syslog.h>
-#include <fcntl.h>
+#include <string.h>
 
-static void skeleton_daemon()
+
+int main(int argc, char* argv[])
 {
-	pid_t pid;
+	FILE *fp= NULL;
+	FILE *fp1= NULL;
+	pid_t process_id = 0;
+	pid_t sid = 0;
+	char * buf = (char *)malloc(sizeof(char)*100);
+	memset (buf,0,sizeof(char)*100);
+	process_id = fork();
 
-	pid = fork();
-
-	if (pid < 0){
-        exit(EXIT_FAILURE);
-	}
-
-	if (pid > 0){
-		exit(EXIT_SUCCESS);
-	}
-
-	if (setsid() < 0){
-		exit(EXIT_FAILURE);
-	}
-    /* Catch, ignore and handle signals */
-    //TODO: Implement a working signal handler */
-	signal(SIGCHLD, SIG_IGN);
-	signal(SIGHUP, SIG_IGN);
-
-    /* Fork off for the second time*/
-	pid = fork();
-
-    /* An error occurred */
-	if (pid < 0){
-		exit(EXIT_FAILURE);
-	}
-    /* Success: Let the parent terminate */
-    	if (pid > 0){
-        	exit(EXIT_SUCCESS);
-	}
-    /* Set new file permissions */
-    	umask(0);
-
-	int archivo = open("log_cpu.log",O_TRUNC | O_CREAT);
-
-	setsid();
-
-	
-    /* Change the working directory to the root directory */
-    /* or another appropriated directory */
-	//chdir("/");
-
-    /* Close all open file descriptors */
-    int x;
-	for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
-    	{
- 	       close (x);
-	}
-
-    /* Open the log file */
-	openlog ("firstdaemon", LOG_PID, LOG_DAEMON);
-}
-
-int main()
-{
-	skeleton_daemon();
-
-	while (1)
+	if (process_id < 0)
 	{
-        	skeleton_daemon();
-        	syslog (LOG_NOTICE, "First daemon started.");
-        	sleep (20);
+		printf("Fallo en el fork\n");
 
-		char * buf = (char *)malloc(sizeof(char)*100);
-
-		FILE *fp = popen("top -bn2 | grep '%Cpu' | tail -1", "r");
-		fread(buf,100,1, fp);
-		puts(buf);
-
-        	break;
+		exit(1);
 	}
 
-	syslog (LOG_NOTICE, "First daemon terminated.");
-	closelog();
-
-	return EXIT_SUCCESS;
+	if (process_id > 0)
+	{
+		printf("Numero del proceso hijo %d \n", process_id);
+		exit(0);
+	}
+	umask(0);
+	sid = setsid();
+	if(sid < 0)
+	{
+		exit(1);
+	}
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+	fp = fopen("log_cpu.log", "w");
+	while (1)
+	{	
+		sleep(2);		
+		fp1 = popen("top -bn2 | grep '%Cpu' | tail -1", "r");
+		fread(buf,100,1, fp1);
+		fwrite(buf, strlen(buf),1,fp);
+		memset (buf,0,sizeof(char)*100);
+		
+	}
+	fclose(fp);
+	return (0);
 }
